8cc4a3cd192f1b63527418e83dfe12213554031f
yohang <yohan@giarel.li>
2017-09-21 11:31:41 +0200

    Added product repository

---
+++ src/Repository/ProductRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @author Yohan Giarelli <yohan@un-zero-un.fr>
+ */
+class ProductRepository extends EntityRepository
+{
+    // …
+}
